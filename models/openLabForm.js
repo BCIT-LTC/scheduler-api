58c665eb16f0bf648fe3f45203d7710e8b0d3c0e
Sam(Gangmin) Bae <sbae000420@gmail.com>
2023-05-11 14:03:36 -0700

    Migrated SQL calls to Prisma ORM call in openLabForm.

--- models/openLabForm.js
+++ models/openLabForm.js
@@ -1,59 +1,47 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
 const dataForm = {
     findMonth: (month) => {
-        let sql = `SELECT * FROM calendar WHERE MONTH(date) = '${month}'`;
-        return new Promise((resolve, reject) => {
-            db.query(sql, (err, results) => {
-                console.log("error and results", err, results);
-                if (err) {
-                    return reject(err);
-                } else {
-                    return resolve(results);
-                }
-            });
+        return prisma.calendar.findMany({
+            where: { date: { contains: month } },
         });
     },
     updateCalendar: (forms) => {
-        const queryArray = forms.map((form) => {
-            return `INSERT INTO calendar VALUES (null, '${form.date}', '${form["start-time"]}', '${form["end-time"]}', '${form["facilitator"]}', '${form["room"]}', '${form["stat"]}' )`;
-        });
         return Promise.all(
-            queryArray.map((sql) => {
-                return new Promise((resolve, reject) => {
-                    db.query(sql, (err, results) => {
-                        console.log("error ANND results", err, results);
-                        if (err) return reject(err);
-
-                        if (results.length !== 0) {
-                            return resolve(results);
-                        }
-                        reject(new Error(`Couldn't find month: '${month}'`));
-                    });
+            forms.map((form) => {
+                return prisma.calendar.upsert({
+                    where: { date: form.date },
+                    create: {
+                        date: form.date.replace(/T/, " ").replace(/Z/, "000"),
+                        start_time: form["start-time"],
+                        end_time: form["end-time"],
+                        facilitator: form["facilitator"],
+                        room: form["room"],
+                        stat: form["stat"],
+                    },
+                    update: {
+                        start_time: form["start-time"],
+                        end_time: form["end-time"],
+                        facilitator: form["facilitator"],
+                        room: form["room"],
+                        stat: form["stat"],
+                    },
                 });
             })
-        ).then((responseArr) => {
-            console.log("response array", responseArr);
-            return responseArr;
-        });
+        );
     },
     updateOpenLabDay: (form) => {
-        let sql = `UPDATE calendar SET date = '${form.date
-            .replace(/T/, " ")
-            .replace(/Z/, "000")}', \`start-time\`= '${
-            form["start-time"]
-        }', \`end-time\` = '${form["end-time"]}', facilitator = '${
-            form["facilitator"]
-        }', room = '${form["room"]}', stat = '${
-            form["stat"]
-        }' WHERE calendar_id = ${form.calendar_id} `;
-        return new Promise((resolve, reject) => {
-            db.query(sql, (err, results) => {
-                console.log("error and results", err, results);
-                if (err) {
-                    return reject(err);
-                } else {
-                    return resolve(results);
-                }
-            });
+        return prisma.calendar.update({
+            where: { calendar_id: form.calendar_id },
+            data: {
+                date: form.date.replace(/T/, " ").replace(/Z/, "000"),
+                start_time: form["start-time"],
+                end_time: form["end-time"],
+                facilitator: form["facilitator"],
+                room: form["room"],
+                stat: form["stat"],
+            },
         });
     },
 };
