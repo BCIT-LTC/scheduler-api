6302e8a4cdebc01a029439a3bc2153e5f3da35ab
Sam(Gangmin) Bae <sbae000420@gmail.com>
2023-05-12 23:27:36 -0700

    Fixed prisma calls for openLabForm

--- models/openLabForm.js
+++ models/openLabForm.js
@@ -2,25 +2,32 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const dataForm = {
-    findMonth: (month) => {
+    findMonth: (date) => {
+        const month = (new Date(date).getMonth() + 1)
+            .toString()
+            .padStart(2, "0");
+
+        const startDate = new Date(`2023-${month}-01T00:00:00.000Z`);
+        const endDate = new Date(`2023-${month}-31T23:59:59.999Z`);
+
         return prisma.calendar.findMany({
-            where: { date: { contains: month } },
+            where: {
+                date: {
+                    gt: startDate,
+                    lt: endDate,
+                },
+            },
         });
     },
     updateCalendar: (forms) => {
         return Promise.all(
             forms.map((form) => {
-                return prisma.calendar.upsert({
-                    where: { date: form.date },
-                    create: {
-                        date: form.date.replace(/T/, " ").replace(/Z/, "000"),
-                        start_time: form["start-time"],
-                        end_time: form["end-time"],
-                        facilitator: form["facilitator"],
-                        room: form["room"],
-                        stat: form["stat"],
-                    },
-                    update: {
+                const date = new Date(
+                    form.date.replace(/T/, " ").replace(/Z/, "000")
+                );
+                return prisma.calendar.create({
+                    data: {
+                        date,
                         start_time: form["start-time"],
                         end_time: form["end-time"],
                         facilitator: form["facilitator"],
@@ -31,11 +38,13 @@ const dataForm = {
             })
         );
     },
+
     updateOpenLabDay: (form) => {
+        const date = new Date(form.date.replace(/T/, " ").replace(/Z/, "000"));
         return prisma.calendar.update({
             where: { calendar_id: form.calendar_id },
             data: {
-                date: form.date.replace(/T/, " ").replace(/Z/, "000"),
+                date,
                 start_time: form["start-time"],
                 end_time: form["end-time"],
                 facilitator: form["facilitator"],
