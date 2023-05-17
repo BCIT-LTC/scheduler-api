e13cbb73074e5926c5e6029ee1ce03a744248aab
Andy Tran <AyyyTran@gmail.com>
2023-05-16 21:14:30 -0700

    added routes for contactModal for ContactUs button

--- app.js
+++ app.js
@@ -1,51 +1,47 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
-const path = require("path");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const path = require('path');
+const cors = require('cors');
 const port = 8000;
 const hostname = '0.0.0.0';
-const overrideMethod = require('method-override')
+const overrideMethod = require('method-override');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(overrideMethod('_method'))
+app.use(overrideMethod('_method'));
 
-const announcements = require("./routes/announcements");
-const auth = require("./routes/auth");
-const calendar = require("./routes/calendar");
+const announcements = require('./routes/announcements');
+const auth = require('./routes/auth');
+const calendar = require('./routes/calendar');
+const contact = require('./routes/contact'); // Import the new contact route file
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-app.use("/", announcements, auth, calendar);
+app.use('/', announcements, auth, calendar, contact); // Add the contact route
 
 const options = {
   definition: {
-    openapi: "3.0.0",
+    openapi: '3.0.0',
     info: {
-      title: "Scheduler-API",
-      version: "dev",
-      description:
-        "Welcome to the API for the BSN Openlab Scheduler",
+      title: 'Scheduler-API',
+      version: 'dev',
+      description: 'Welcome to the API for the BSN Openlab Scheduler',
     },
     servers: [
       {
-        url: "http://localhost:8000",
+        url: 'http://localhost:8000',
       },
     ],
   },
-  apis: ["./routes/*.js"],
+  apis: ['./routes/*.js'],
 };
 
 const specs = swaggerJsdoc(options);
-app.use(
-  "/api",
-  swaggerUi.serve,
-  swaggerUi.setup(specs, { explorer: true })
-);
+app.use('/api', swaggerUi.serve, swaggerUi.setup(specs, {explorer: true}));
 
 app.listen(port, hostname, () => {
   console.log(`Server started on port ${port}`);
