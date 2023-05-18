018d12ce99190f2c19a4ef9ea71b7a656535be03
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2023-05-18 16:00:12 -0700

    add test cases for login and logout routes

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,27 +1,46 @@
 const request = require('supertest');
 const app = require('../app');
+const utilities = require('./utilities')
+const {
+  localAdmin,
+  admin,
+  student
+} = utilities;
 
-describe("GET /api/announcement", () => {
-  test("should return all announcements", async () => {
-    const res1 = await request(app).get("/api/announcement");
-    console.log(res1.status)
-    expect(res1.statusCode).toBe(403);
-    expect(res1.body.length).toBe(0);
+describe("login", () => {
+  test("If the user has issued a jwt", async () => {
+    // with jwt
+    const endpoint = '/api/login';
+    const res1 = await request(app).get(endpoint).set({
+      'Authorization':localAdmin
+    });
+    console.log(res1)
+    expect(res1.statusCode).toBe(200);
 
-    const res2 = await request(app).get("/api/announcement");
-    console.log(res2.status)
-    expect(res2.statusCode).toBe(200);
-    expect(res2.body.length).toBeGreaterThan(0);
+    // w/o jwt
+    const res2 = await request(app).get(endpoint);
+    console.log(res2)
+    expect(res2.statusCode).toBe(403);
+    
+    // for normal user
   });
 });
 
-// describe('Photos endpoint', () => {
-//   test('should return hello world object', async () => {
-//     const res = await request(app)
-//       .get('/api/photos')
-//     expect(res.statusCode).toEqual(200)
-//     expect(res.body).toEqual({
-//       message: "Hello World"
-//     })
-//   })
-// })
+describe("logout", () => {
+  test("If the user has issued a jwt", async () => {
+    // with jwt
+    const endpoint = '/api/logouttime';
+    const res1 = await request(app).get(endpoint).set({
+      'Authorization':localAdmin
+    });
+    console.log(res1)
+    expect(res1.statusCode).toBe(200);
+
+    // w/o jwt
+    const res2 = await request(app).get(endpoint);
+    console.log(res2)
+    expect(res2.statusCode).toBe(403);
+    
+    // for normal user
+  });
+});
---
+++ tests/utilities.js
@@ -0,0 +1,6 @@
+module.exports = {
+  localAdmin: process.env.LOCAL_ADMIN_TOKEN,
+  admin: process.env.LOCAL_ADMIN_TOKEN,
+  student: process.env.STUDENT_TOKEN,
+  prefix: 'api'
+}
