84d2d91fad4c06fad580bff9a80745a43858d997
Sam(Gangmin) Bae <sbae000420@gmail.com>
2023-05-17 20:29:28 -0700

    Changed database calls to try and catch

--- models/openLabForm.js
+++ models/openLabForm.js
@@ -10,40 +10,54 @@ const dataForm = {
         const startDate = new Date(`2023-${month}-01T00:00:00.000Z`);
         const endDate = new Date(`2023-${month}-31T23:59:59.999Z`);
 
-        return prisma.calendar.findMany({
-            where: {
-                date: {
-                    gt: startDate,
-                    lt: endDate,
+        try {
+            const calendar = prisma.calendar.findMany({
+                where: {
+                    date: {
+                        gt: startDate,
+                        lt: endDate,
+                    },
                 },
-            },
-        });
+            });
+            return calendar;
+        } catch (error) {
+            console.log(
+                "An error ocurred while getting the calendar: " + error
+            );
+        }
     },
-    updateCalendar: (forms) => {
-        return Promise.all(
-            forms.map((form) => {
-                const date = new Date(form.date);
-                date.setUTCHours(0, 0, 0, 0); // Set the time component to 00:00:00.000
-                return prisma.calendar.upsert({
-                    where: { date },
-                    create: {
-                        date,
-                        start_time: form["start-time"],
-                        end_time: form["end-time"],
-                        facilitator: form["facilitator"],
-                        room: form["room"],
-                        stat: form["stat"],
-                    },
-                    update: {
-                        start_time: form["start-time"],
-                        end_time: form["end-time"],
-                        facilitator: form["facilitator"],
-                        room: form["room"],
-                        stat: form["stat"],
-                    },
-                });
-            })
-        );
+    updateCalendar: async (forms) => {
+        try {
+            return await Promise.all(
+                forms.map(async (form) => {
+                    const date = new Date(form.date);
+                    date.setUTCHours(0, 0, 0, 0); // Set the time component to 00:00:00.000
+                    return await prisma.calendar.upsert({
+                        where: { date },
+                        create: {
+                            date,
+                            start_time: form["start-time"],
+                            end_time: form["end-time"],
+                            facilitator: form["facilitator"],
+                            room: form["room"],
+                            stat: form["stat"],
+                        },
+                        update: {
+                            start_time: form["start-time"],
+                            end_time: form["end-time"],
+                            facilitator: form["facilitator"],
+                            room: form["room"],
+                            stat: form["stat"],
+                        },
+                    });
+                })
+            );
+        } catch (error) {
+            console.error(
+                "An error occurred while updating the calendar:",
+                error
+            );
+        }
     },
 };
 
