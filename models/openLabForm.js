c883a5c81eb0feaa01c3af7108386f128e91fdfd
Sam Bredenhof <sambredenhof@gmail.com>
2023-05-17 22:07:28 -0700

    more specific request

--- models/openLabForm.js
+++ models/openLabForm.js
@@ -2,18 +2,18 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const dataForm = {
-    findMonth: (date) => {
+    findMonth: (date, year) => {
         const month = (new Date(date).getMonth() + 1)
             .toString()
             .padStart(2, "0");
-
-        const startDate = new Date(`2023-${month}-01T00:00:00.000Z`);
-        const endDate = new Date(`2023-${month}-31T23:59:59.999Z`);
+        console.log(year);
+        const startDate = new Date(`${year}-${month}-01T00:00:00.000Z`);
+        const endDate = new Date(`${year}-${month}-31T23:59:59.999Z`);
 
         return prisma.calendar.findMany({
             where: {
                 date: {
-                    gt: startDate,
+                    gte: startDate,
                     lt: endDate,
                 },
             },
@@ -24,19 +24,21 @@ const dataForm = {
             forms.map((form) => {
                 const date = new Date(form.date);
                 date.setUTCHours(0, 0, 0, 0); // Set the time component to 00:00:00.000
+                console.log(form);
+                if (!form.calendar_id) form.calendar_id = -1;
                 return prisma.calendar.upsert({
-                    where: { date },
+                    where: { calendar_id: form.calendar_id },
                     create: {
                         date,
-                        start_time: form["start-time"],
-                        end_time: form["end-time"],
+                        start_time: form["start_time"],
+                        end_time: form["end_time"],
                         facilitator: form["facilitator"],
                         room: form["room"],
                         stat: form["stat"],
                     },
                     update: {
-                        start_time: form["start-time"],
-                        end_time: form["end-time"],
+                        start_time: form["start_time"],
+                        end_time: form["end_time"],
                         facilitator: form["facilitator"],
                         room: form["room"],
                         stat: form["stat"],
