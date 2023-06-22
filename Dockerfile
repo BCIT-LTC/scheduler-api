3aaddaf2cc6a8e878136ff25e169eaeb8dc40021
Kyle Hunter <kyle_hunter@bcit.ca>
2023-06-22 16:53:25 -0700

    fix: updates docker-compose; Dockerfile tweaks

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,23 @@
 FROM node:19.4.0-alpine
 
+WORKDIR /app
+
 RUN apk --update add \
         mariadb-client \
         curl \
     ;
 
-WORKDIR /app
-
-COPY middleware ./middleware
-COPY models ./models
-COPY routes ./routes
-# COPY views ./views
-COPY app.js ./
 COPY package.json ./
 
 RUN npm install
 
+COPY ./middleware ./models ./prisma ./routes app.js package.json ./
+
 COPY docker-entrypoint.sh /usr/local/bin
+RUN chmod +x /usr/local/bin/docker-entrypoint.sh
 
 EXPOSE 8000
 
 ENTRYPOINT ["docker-entrypoint.sh"]
 
-# HEALTHCHECK --interval=1m30s --timeout=10s \
-#   CMD curl -f http://localhost:8080/ || exit 1
-
-CMD ["npm", "start"]
+CMD ["npm", "prod"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,23 +7,8 @@ if [ -z "$MARIADB_ROOT_HOST" ] || [ -z "$MARIADB_USER" ] || [ -z "$MARIADB_PASSW
     exit 1
 fi
 
-# Environment variable trace
-if [ "${DEBUG}" == "true" ]; then echo -e " \
-    \nEnvironment Variables: \
-    \n  MARIADB_ROOT_HOST: $MARIADB_ROOT_HOST \
-    \n  MARIADB_USER: $MARIADB_USER \
-    \n  MARIADB_PASSWORD: $MARIADB_PASSWORD \
-    \n  MARIADB_DATABASE: $MARIADB_DATABASE \
-    \n  APP_URL: $APP_URL \
-    \n  GIT_TAG: $GIT_TAG \
-    \n";
-fi
-# Configure and store connection info
->&2 echo "Writing ENV VARS to .env file..."
-echo -n "" > .env
-echo DATABASE_URL="mysql://$MARIADB_USER:$MARIADB_PASSWORD@$MARIADB_ROOT_HOST:3306/$MARIADB_DATABASE" >> .env
-echo APP_URL=$APP_URL >> .env
-
+# Configure connection info
+export DATABASE_URL="mysql://$MARIADB_USER:$MARIADB_PASSWORD@$MARIADB_ROOT_HOST:3306/$MARIADB_DATABASE"
 
 # Initialize prisma
 >&2 echo "Running migrations..."
@@ -36,8 +21,8 @@ npx prisma db seed
 # Return to parent shell to run app
 >&2 echo "Starting app..."
 
-# run tests
->&2 echo "Running Tests..."
-npm run test
+# # run tests
+# >&2 echo "Running Tests..."
+# npm run test
 
 exec "$@"
