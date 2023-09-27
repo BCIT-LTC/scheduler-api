95df432ba4d799e468b773c2166f3b05449becce
Kyle Hunter <kyle_hunter@bcit.ca>
2023-09-26 22:16:31 -0700

    initial migration to vso

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-# Get secrets from Vault init container (latest/stable environments only) and set as ENV VARS
-if [ -f "/vault/secrets/config" ]; then grep -v -e '^#' -e '^[[:space:]]*$' /vault/secrets/config > .env; 
-set -o allexport && source .env && set +o allexport;fi
-
-printenv
 # Verify that the minimally required environment variables are set.
 if [ -z "$MARIADB_ROOT_HOST" ] || [ -z "$MARIADB_USER" ] || [ -z "$MARIADB_PASSWORD" ] || [ -z "$MARIADB_DATABASE" ] || [ -z "$SECRET_KEY" ]; then
     printf '\n\nEnvironment variables are not set.\n\tYou need to specify MARIADB_ROOT_HOST, MARIADB_DATABASE, MARIADB_USER, MARIADB_PASSWORD and SECRET_KEY\n\n'
