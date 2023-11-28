1f26f0aed6a236aee5d4f0baabaee09561ccecf6
Viresh Soedhwa <vsoedhwa@bcit.ca>
2023-11-28 12:40:17 -0800

    check with completely different dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
-FROM node:19.4.0
+FROM python:3.11-slim AS release
+
 LABEL maintainer courseproduction@bcit.ca
+
+ENV PYTHONUNBUFFERED 1
+ENV PATH /code:/opt/venv/bin:$PATH
 ARG VERSION
 ENV VERSION=${VERSION:-0.0.0}
+# FROM node:19.4.0
+# LABEL maintainer courseproduction@bcit.ca
+# ARG VERSION
+# ENV VERSION=${VERSION:-0.0.0}
 
 WORKDIR /app
 
@@ -11,19 +19,17 @@ WORKDIR /app
 #         curl \
 #     ;
 
-RUN apt-get update -y && apt-get install mariadb-client curl -y
+# # Copy app
+# COPY package.json ./
 
-# Copy app
-COPY package.json ./
+# RUN npm install
 
-RUN npm install
-
-COPY middleware ./middleware/
-COPY models ./models/
-COPY prisma ./prisma/
-COPY routes ./routes/
-COPY app.js ./
-COPY logger.js ./
+# COPY middleware ./middleware/
+# COPY models ./models/
+# COPY prisma ./prisma/
+# COPY routes ./routes/
+# COPY app.js ./
+# COPY logger.js ./
 
 # Copy and run init script
 COPY docker-entrypoint.sh /usr/local/bin/
@@ -33,4 +39,4 @@ ENTRYPOINT ["/usr/local/bin/docker-entrypoint.sh"]
 
 EXPOSE 8000
 
-CMD ["npm", "run", "prod"]
+CMD ["tail", "-f", "/dev/null"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,25 +3,25 @@ set -e
 
 echo "Running entrypoint script..."
 echo "Version: $VERSION"
-# Verify that the minimally required environment variables are set.
-if [ -z "$MARIADB_ROOT_HOST" ] || [ -z "$MARIADB_USER" ] || [ -z "$MARIADB_PASSWORD" ] || [ -z "$MARIADB_DATABASE" ] || [ -z "$JWT_AUTH_SIGNING_KEY" ]; then
-    printf '\n\nEnvironment variables are not set.\n\tYou need to specify MARIADB_ROOT_HOST, MARIADB_DATABASE, MARIADB_USER, MARIADB_PASSWORD and JWT_AUTH_SIGNING_KEY\n\n'
-    exit 1
-fi
+# # Verify that the minimally required environment variables are set.
+# if [ -z "$MARIADB_ROOT_HOST" ] || [ -z "$MARIADB_USER" ] || [ -z "$MARIADB_PASSWORD" ] || [ -z "$MARIADB_DATABASE" ] || [ -z "$JWT_AUTH_SIGNING_KEY" ]; then
+#     printf '\n\nEnvironment variables are not set.\n\tYou need to specify MARIADB_ROOT_HOST, MARIADB_DATABASE, MARIADB_USER, MARIADB_PASSWORD and JWT_AUTH_SIGNING_KEY\n\n'
+#     exit 1
+# fi
 
-# Configure connection info
->&2 echo "Configuring DATABASE_URL...\n"
-export DATABASE_URL="mysql://$MARIADB_USER:$MARIADB_PASSWORD@$MARIADB_ROOT_HOST:3306/$MARIADB_DATABASE"
+# # Configure connection info
+# >&2 echo "Configuring DATABASE_URL...\n"
+# export DATABASE_URL="mysql://$MARIADB_USER:$MARIADB_PASSWORD@$MARIADB_ROOT_HOST:3306/$MARIADB_DATABASE"
 
-# Initialize prisma
->&2 echo "Running migrations...\n"
-npx prisma db push --schema='prisma/schema.prisma'
+# # Initialize prisma
+# >&2 echo "Running migrations...\n"
+# npx prisma db push --schema='prisma/schema.prisma'
 
-# Seed database with initial data
->&2 echo "Seeding db...\n"
-npx prisma db seed
+# # Seed database with initial data
+# >&2 echo "Seeding db...\n"
+# npx prisma db seed
 
-# Return to parent shell to run app
->&2 echo "Starting app...\n"
+# # Return to parent shell to run app
+# >&2 echo "Starting app...\n"
 
 exec "$@"
