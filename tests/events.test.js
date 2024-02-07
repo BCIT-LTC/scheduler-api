ccdfafa33150b1670da42be4f1116232dcead23d
Brandon Chan <brandonchan45@gmail.com>
2024-02-06 20:34:32 -0800

    minor updates to events.test.js

--- tests/events.test.js
+++ tests/events.test.js
@@ -1,14 +1,29 @@
 const request = require("supertest");
 const app = require("../app");
 const utilities = require("./utilities");
-const endpoint1 = "localhost:8000/api/events";
+const { localAdmin, student, admin } = utilities;
+const endpoint = "/api/events/day";
 
 describe("GET all events", () => {
-  test("should return all events on the day with optional date parameter", async () => {
-    const endpoint = `${endpoint1}/day?date=2024-01-02`;
-    const res = await request(app).get(endpoint);
+  it("should return all events on the day with optional date parameter", async () => {
+    const testData = [
+      {
+        event_id: 1,
+        start_time: new Date("2024-01-02T08:30:00"),
+        end_time: new Date("2024-01-02T14:30:00"),
+        summary: "Event 1",
+        description: "Event 1 Description",
+      },
+    ];
+    const res = await request(app)
+      .get(endpoint)
+      .query({ date: "2024-01-02" })
+      .set({
+        Authorization: localAdmin,
+      });
     expect(res.statusCode).toBe(200);
     console.log(res.body);
-    // expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body).toEqual(testData);
   });
 });
