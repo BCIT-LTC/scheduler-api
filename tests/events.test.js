0aff4d220e9da17b67bab0aeb0d769e499721db7
lestershx <lshun@my.bcit.ca>
2024-02-07 02:37:00 -0800

    started get all events by month tests

--- tests/events.test.js
+++ tests/events.test.js
@@ -10,3 +10,19 @@ describe("GET all events", () => {
     expect(res.body.length).toBeGreaterThan(0);
   });
 });
+
+describe("GET all events by month", () => {
+  const endpoint = "/api/events/month";
+
+  test("should return events for a given month", async () => {
+    let date = new Date("2024-02-20");
+
+    const res = await request(app)
+      .get(endpoint)
+      .query({ date });
+
+    expect(res.statusCode).toBe(200);
+    // expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body.length).toBe(4);
+  });
+});
