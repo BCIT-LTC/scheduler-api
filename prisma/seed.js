87b589abdb3e3f6ec020627f8680031004210f95
Stanley Chow <sjchow97@gmail.com>
2024-02-07 22:15:56 -0800

    removed routes for faqs/calendar in app.js and associated seeder functions

--- prisma/seed.js
+++ prisma/seed.js
@@ -10,42 +10,20 @@ const fs = require("fs");
  * @returns {Object} seeded announcement data
  */
 async function seedAnnouncements() {
-  try {
-    return await prisma.announcements.upsert({
-      where: { announcements_id: 1 },
-      update: {},
-      create: {
-        title: "Welcome!",
-        description: "Welcome to the BSN OpenLab Scheduler!",
-        date: new Date(),
-      },
-    });
-  } catch (error) {
-    logger.error({ message: "seedAnnouncements", error: error.stack });
-    throw error;
-  }
-}
-
-/**
- * Seed initial faq data.
- * @async
- * @returns {Object} seeded faq data
- */
-async function seedFaqs() {
-  try {
-    return await prisma.faqs.upsert({
-      where: { faqs_id: 1 },
-      update: {},
-      create: {
-        question: "What is Open Lab?",
-        answer:
-          "Open Lab is a space for BSN students to practice their psychomotor skills",
-      },
-    });
-  } catch (error) {
-    logger.error({ message: "seedFaqs", error: error.stack });
-    throw error;
-  }
+    try {
+        return await prisma.announcements.upsert({
+            where: { announcements_id: 1 },
+            update: {},
+            create: {
+                title: "Welcome!",
+                description: "Welcome to the BSN OpenLab Scheduler!",
+                date: new Date(),
+            },
+        });
+    } catch (error) {
+        logger.error({ message: "seedAnnouncements", error: error.stack });
+        throw error;
+    }
 }
 
 /**
@@ -55,36 +33,36 @@ async function seedFaqs() {
  * @return {Array} of seeded event data
  */
 async function seedEvents() {
-  const seedPath = "./prisma/seedData/events.json";
-  try {
-    fs.readFile(seedPath, "utf8", async (err, data) => {
-      if (err) {
-        console.error("Error reading the file:", err);
-        return;
-      }
+    const seedPath = "./prisma/seedData/events.json";
+    try {
+        fs.readFile(seedPath, "utf8", async(err, data) => {
+            if (err) {
+                console.error("Error reading the file:", err);
+                return;
+            }
 
-      // 'data' is the content of the file as a string
-      const jsonData = JSON.parse(data);
+            // 'data' is the content of the file as a string
+            const jsonData = JSON.parse(data);
 
-      // Insert data into the database
-      for (const event of jsonData) {
-        await prisma.events.upsert({
-          where: { event_id: event.event_id },
-          update: {},
-          create: {
-            start_time: new Date(event.start_time),
-            end_time: new Date(event.end_time),
-            summary: event.summary,
-            description: event.description,
-            status: event.status,
-          },
+            // Insert data into the database
+            for (const event of jsonData) {
+                await prisma.events.upsert({
+                    where: { event_id: event.event_id },
+                    update: {},
+                    create: {
+                        start_time: new Date(event.start_time),
+                        end_time: new Date(event.end_time),
+                        summary: event.summary,
+                        description: event.description,
+                        status: event.status,
+                    },
+                });
+            }
         });
-      }
-    });
-  } catch (error) {
-    logger.error({ message: "seedEvents", error: error.stack });
-    throw error;
-  }
+    } catch (error) {
+        logger.error({ message: "seedEvents", error: error.stack });
+        throw error;
+    }
 }
 
 /**
@@ -92,16 +70,15 @@ async function seedEvents() {
  * @async
  */
 async function seedDatabase() {
-  try {
-    await seedAnnouncements();
-    await seedFaqs();
-    await seedEvents();
-    console.log("Seeding completed successfully.");
-  } catch (error) {
-    logger.error({ message: "seedDatabase", error: error.stack });
-  } finally {
-    await prisma.$disconnect();
-  }
+    try {
+        await seedAnnouncements();
+        await seedEvents();
+        console.log("Seeding completed successfully.");
+    } catch (error) {
+        logger.error({ message: "seedDatabase", error: error.stack });
+    } finally {
+        await prisma.$disconnect();
+    }
 }
 
 seedDatabase().then((r) => console.log(r));
