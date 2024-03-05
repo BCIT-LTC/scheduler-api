5b4d721b1daf011a6190c44a9f0e5177dce3ba50
Brandon Chan <brandonchan45@gmail.com>
2024-03-05 09:39:10 -0800

    fixed issue with parseArgs import

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,19 +1,16 @@
-import { parseArgs } from 'node:util'
-
+const { parseArgs } = require("node:util");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const createLogger = require("../logger"); // Ensure the path is correct
 const logger = createLogger(module);
-const fs = require("fs");
 const environment = process.env.NODE_ENV;
-
 
 /**
  * CLI options for the seed script.
  */
 const options = {
-    environment: { type: 'string' },
-}
+  environment: { type: "string" },
+};
 
 /**
  * Initial location data from seedData/locations.js
@@ -31,20 +28,20 @@ const events = require("./seedData/events");
  * @returns {Object} seeded announcement data
  */
 async function seedAnnouncements() {
-    try {
-        return await prisma.announcements.upsert({
-            where: { announcements_id: 1 },
-            update: {},
-            create: {
-                title: "Welcome!",
-                description: "Welcome to the BSN OpenLab Scheduler!",
-                date: new Date(),
-            },
-        });
-    } catch (error) {
-        logger.error({ message: "seedAnnouncements", error: error.stack });
-        throw error;
-    }
+  try {
+    return await prisma.announcements.upsert({
+      where: { announcements_id: 1 },
+      update: {},
+      create: {
+        title: "Welcome!",
+        description: "Welcome to the BSN OpenLab Scheduler!",
+        date: new Date(),
+      },
+    });
+  } catch (error) {
+    logger.error({ message: "seedAnnouncements", error: error.stack });
+    throw error;
+  }
 }
 
 /**
@@ -54,51 +51,51 @@ async function seedAnnouncements() {
  * @return {Array} of seeded event data
  */
 async function seedEvents() {
-    try {
-        const eventArrayHalf = Math.floor(events.length / 2);
+  try {
+    const eventArrayHalf = Math.floor(events.length / 2);
 
-        // Half of the events will have the 1st location
-        for (let i = 0; i < eventArrayHalf; i++) {
-            const event = events[i];
-            await prisma.events.upsert({
-                where: { event_id: event.event_id },
-                update: {},
-                create: {
-                    start_time: new Date(event.start_time),
-                    end_time: new Date(event.end_time),
-                    summary: event.summary,
-                    description: event.description,
-                    created: new Date(),
-                    status: event.status,
-                    location: {
-                        connect: { location_id: locations[0].location_id },
-                    },
-                },
-            });
-        }
-
-        // Half of the events will have the 2nd location
-        for (let i = eventArrayHalf + 1; i < events.length; i++) {
-            const event = events[i];
-            await prisma.events.upsert({
-                where: { event_id: event.event_id },
-                update: {},
-                create: {
-                    start_time: new Date(event.start_time),
-                    end_time: new Date(event.end_time),
-                    summary: event.summary,
-                    description: event.description,
-                    status: event.status,
-                    location: {
-                        connect: { location_id: locations[1].location_id },
-                    },
-                },
-            });
-        }
-    } catch (error) {
-        logger.error({ message: "seedEvents", error: error.stack });
-        throw error;
+    // Half of the events will have the 1st location
+    for (let i = 0; i < eventArrayHalf; i++) {
+      const event = events[i];
+      await prisma.events.upsert({
+        where: { event_id: event.event_id },
+        update: {},
+        create: {
+          start_time: new Date(event.start_time),
+          end_time: new Date(event.end_time),
+          summary: event.summary,
+          description: event.description,
+          created: new Date(),
+          status: event.status,
+          location: {
+            connect: { location_id: locations[0].location_id },
+          },
+        },
+      });
     }
+
+    // Half of the events will have the 2nd location
+    for (let i = eventArrayHalf + 1; i < events.length; i++) {
+      const event = events[i];
+      await prisma.events.upsert({
+        where: { event_id: event.event_id },
+        update: {},
+        create: {
+          start_time: new Date(event.start_time),
+          end_time: new Date(event.end_time),
+          summary: event.summary,
+          description: event.description,
+          status: event.status,
+          location: {
+            connect: { location_id: locations[1].location_id },
+          },
+        },
+      });
+    }
+  } catch (error) {
+    logger.error({ message: "seedEvents", error: error.stack });
+    throw error;
+  }
 }
 
 /**
@@ -107,21 +104,21 @@ async function seedEvents() {
  * @return {Object} seed location data
  */
 async function seedLocations() {
-    try {
-        // Insert locations from seedData/locations.json into the database
-        for (let i = 0; i < locations.length; i++) {
-            await prisma.locations.upsert({
-                where: { location_id: locations[i].location_id },
-                update: {},
-                create: {
-                    room_number: locations[i].room_number,
-                },
-            });
-        }
-    } catch (error) {
-        logger.error({ message: "seedLocations", error: error.stack });
-        throw error;
+  try {
+    // Insert locations from seedData/locations.json into the database
+    for (let i = 0; i < locations.length; i++) {
+      await prisma.locations.upsert({
+        where: { location_id: locations[i].location_id },
+        update: {},
+        create: {
+          room_number: locations[i].room_number,
+        },
+      });
     }
+  } catch (error) {
+    logger.error({ message: "seedLocations", error: error.stack });
+    throw error;
+  }
 }
 
 /**
@@ -129,34 +126,32 @@ async function seedLocations() {
  * @async
  */
 async function seedDatabase() {
+  const {
+    values: { environment },
+  } = parseArgs({ options });
 
-    const {
-        values: { environment },
-    } = parseArgs({ options })
+  try {
+    switch (environment) {
+      case "development":
+        // add development AND production seed data
+        // seedAnnouncements();
+        seedLocations();
+        seedEvents();
+        break;
+      case "test":
+        // add test seed data, running all test scripts at once
 
-    try {
-        switch (environment) {
-            case "development":
-                // add development AND production seed data
-                // seedAnnouncements();
-                seedLocations();
-                seedEvents();
-                break;
-            case "test":
-                // add test seed data, running all test scripts at once
-
-                break;
-            default:
-                // add production seed data (defaults to production if no environment is specified)
-                seedLocations();
-                break;
-        }
-    } catch (error) {
-        logger.error({ message: "seedDatabase", error: error.stack });
-    } finally {
-        await prisma.$disconnect();
+        break;
+      default:
+        // add production seed data (defaults to production if no environment is specified)
+        seedLocations();
+        break;
     }
-
+  } catch (error) {
+    logger.error({ message: "seedDatabase", error: error.stack });
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 seedDatabase().then((r) => console.log(r));
