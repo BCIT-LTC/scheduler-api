d95769da840cf40206a77c1564c2cbb4a236536c
Brandon Chan <brandonchan45@gmail.com>
2024-03-19 11:53:29 -0700

    added test case for invalid token

--- tests/routes/auth.test.js
+++ tests/routes/auth.test.js
@@ -13,11 +13,23 @@ describe("Test suite for the auth routes", () => {
         first_name: "Test",
         last_name: "User",
         saml_role: "student",
-        app_role: "student",
         school: "Test School",
         program: "Test Program",
         isActive: true,
       });
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("Should return 200 for an invalid token", async () => {
+    const response = await request(app).post("/authorize").send({
+      email: "test1@test.com",
+      first_name: "Test1",
+      last_name: "User",
+      saml_role: "student",
+      school: "Test School",
+      program: "Test Program",
+      isActive: true,
+    });
     expect(response.statusCode).toBe(200);
   });
 });
