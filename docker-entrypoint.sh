14ef4ea2dcd0ce59522cf46da8045a64530592fe
Viresh Soedhwa <vsoedhwa@bcit.ca>
2024-05-06 08:39:54 -0700

    Resolve "replace mariadb with postgres"

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,14 @@
 set -e
 
 # Verify that the minimally required environment variables are set.
-if [ -z "$MARIADB_ROOT_HOST" ] || [ -z "$MARIADB_USER" ] || [ -z "$MARIADB_PASSWORD" ] || [ -z "$MARIADB_DATABASE" ] || [ -z "$JWT_AUTH_SIGNING_KEY" ]; then
-    printf '\n\nEnvironment variables are not set.\n\tYou need to specify MARIADB_ROOT_HOST, MARIADB_DATABASE, MARIADB_USER, MARIADB_PASSWORD and JWT_AUTH_SIGNING_KEY\n\n'
+if [ -z "$POSTGRES_HOST" ] || [ -z "$POSTGRES_USER" ] || [ -z "$POSTGRES_PASSWORD" ] || [ -z "$POSTGRES_DB" ] || [ -z "$JWT_AUTH_SIGNING_KEY" ]; then
+    printf '\n\nEnvironment variables are not set.\n\tYou need to specify POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB and JWT_AUTH_SIGNING_KEY\n\n'
     exit 1
 fi
 
 # Configure connection info
 >&2 echo "Configuring DATABASE_URL...\n"
-export DATABASE_URL="mysql://$MARIADB_USER:$MARIADB_PASSWORD@$MARIADB_ROOT_HOST:3306/$MARIADB_DATABASE"
+export DATABASE_URL="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:5432/$POSTGRES_DB"
 
 # update the prisma schema
 >&2 echo "Updating prisma schema...\n"
